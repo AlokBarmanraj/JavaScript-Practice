97fcb907ca2e2d6ef407174942400feebfbbebda
alok <alokbarmanraj2021@gmail.com>
2026-01-27 16:27:26 +0600

    first commit

---
+++ for-loop.js
@@ -0,0 +1,15 @@
+// for(let num = 0; num < 10; num++){
+//     console.log(num);
+// }
+
+
+// odd 
+for(let i = 0; i< 20; i=i+2){
+    console.log(i);
+}
+
+
+// even 
+for (let j=1; j<20; j=j+2){
+    console.log(j);
+}
---
+++ while.js
@@ -0,0 +1,47 @@
+let number= 1;
+while (number<10){
+    console.log('looping',number);
+    number++
+}
+
+
+
+let num = 1;
+while(num<=10){
+    console.log(num);
+    num=num+1
+}
+
+
+// 1 task 
+let message =1;
+while(message<=60){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+    message++
+}
+
+
+// 2 task 
+// Odd 
+let odd =61
+while(odd<=100){
+    console.log(odd);
+    if(odd % 2 !== 0){
+        console.log('odd number',odd);
+    }
+    odd++
+}
+
+// Even 
+let even =61;
+while(even<=100){
+    console.log(even);
+    if(even%2 ===0){
+        console.log('even number',even);
+    }
+    even++
+}
+
+
+
+// 3 task 
